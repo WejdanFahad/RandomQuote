f8c6c94c303418608fed5935094ae6f691edcd5b
Wejdan <wejdan.303@gmail.com>
2020-06-26 23:06:00 +0300

    Initial Commit

---
+++ RandomQuote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomQuote
+//
+//  Created by Wejdan on 05/11/1441 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
