d423c0dc2c2df7b65bd4304802e4547d0d3d0fd9
Wejdan <wejdan.303@gmail.com>
2020-08-15 15:37:24 +0300

    fix indentation problems

--- RandomQuote/controller/QuoteTableViewController.swift
+++ RandomQuote/controller/QuoteTableViewController.swift
@@ -9,56 +9,56 @@ import UIKit
 import CoreData
 
 class QuoteTableViewController: UITableViewController  {
-
+    
     
     var fetchedResultsController: NSFetchedResultsController<FevorateQuote>!
-
+    
     override func viewDidLoad() {
-           super.viewDidLoad()
-            fetchSavedQuotes()
-        }
+        super.viewDidLoad()
+        fetchSavedQuotes()
+    }
     
-
-        func fetchSavedQuotes() {
-            let fetchRequest: NSFetchRequest<FevorateQuote> = FevorateQuote.fetchRequest()
-            let sortDescriptor = NSSortDescriptor(key: "id", ascending: true)
-            fetchRequest.sortDescriptors = [sortDescriptor]
-
-            fetchedResultsController = NSFetchedResultsController(
-                fetchRequest: fetchRequest,
-                managedObjectContext: DataController.shared.viewContext,
-                sectionNameKeyPath: nil,
-                cacheName: nil
-            )
-            fetchedResultsController.delegate = self
-            do {
-                try fetchedResultsController.performFetch()
-                
-            } catch {
-                self.showAlert(K.errorMessage)
-                
-            }
-        }
-
     
-
+    func fetchSavedQuotes() {
+        let fetchRequest: NSFetchRequest<FevorateQuote> = FevorateQuote.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "id", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        fetchedResultsController = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: DataController.shared.viewContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        )
+        fetchedResultsController.delegate = self
+        do {
+            try fetchedResultsController.performFetch()
+            
+        } catch {
+            self.showAlert(K.errorMessage)
+            
+        }
+    }
+    
+    
+    
     // MARK: - Table view data source
-
-
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-
+        
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! QuoteCell
-
+        
         let qoute = fetchedResultsController.object(at: indexPath)
-
+        
         cell.quote = qoute
-
+        
         return cell
     }
     
@@ -72,20 +72,20 @@ class QuoteTableViewController: UITableViewController  {
             break
         }
     }
-  
+    
 }
 
 
 // MARK: - Fetched Results Controller Delegate
 
 extension QuoteTableViewController: NSFetchedResultsControllerDelegate {
-        
-
+    
+    
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-      
-       tableView.reloadData()
+        
+        tableView.reloadData()
         
     }
     
-
+    
 }
